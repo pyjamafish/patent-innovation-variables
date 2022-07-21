9987c523dc7d79657667024e1dddd01e44b6ff41
Fisher Sun <fisher521.fs@gmail.com>
2022-07-21 14:50:56 -0400

    Absorb conftest into test_citations.py

--- tests/test_citations.py
+++ tests/test_citations.py
@@ -1,5 +1,33 @@
+from importlib import resources
+
 import polars as pl
+import pytest
+
 from patent_analysis import citations
+
+
+def get_filename_in_citations_package(basename: str) -> str:
+    return f"{resources.files('tests.fixtures.citations')}/{basename}"
+
+
+@pytest.fixture()
+def citations_patent_path():
+    return get_filename_in_citations_package("patent.tsv")
+
+
+@pytest.fixture()
+def citations_sample_path():
+    return get_filename_in_citations_package("sample.csv")
+
+
+@pytest.fixture()
+def citations_uspatentcitation_path():
+    return get_filename_in_citations_package("uspatentcitation.tsv")
+
+
+@pytest.fixture()
+def citations_expected_output_path():
+    return get_filename_in_citations_package("output.tsv")
 
 
 def test_citation(
